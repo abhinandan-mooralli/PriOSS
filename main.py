f9e26e5207467e8319fd2c76e9d225f6475dd4c8
Abhinandan Mooralli <69682155+abhinandan-mooralli@users.noreply.github.com>
2023-09-22 22:50:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -25,9 +25,11 @@ notebook = ttk.Notebook(root)
 tab1 = ttk.Frame(notebook)
 tab2 = ttk.Frame(notebook)
 tab3 = ttk.Frame(notebook)
+tab4 = ttk.Frame(notebook)
 notebook.add(tab1, text='User Info')
 notebook.add(tab2, text='Streaming History')
-notebook.add(tab3, text='Inferences')
+notebook.add(tab3, text='Top 10 artists')
+notebook.add(tab4, text='Inferences')
 notebook.pack()
 
 # Tab 1: User Info
@@ -48,18 +50,38 @@ streaming_history_df = streaming_history_df.sort_values(by='endTime')
 
 fig = Figure(figsize=(8, 4))
 ax = fig.add_subplot(111)
-ax.plot(streaming_history_df['endTime'], streaming_history_df['msPlayed'], marker='o', linestyle='-')
+ax.plot(streaming_history_df['endTime'], streaming_history_df['msPlayed'], marker='o', linestyle='')
 ax.set_title('Streaming History')
-ax.set_xlabel('Date & Time')
+ax.set_xlabel('Date')
 ax.set_ylabel('Milliseconds Played')
 canvas = FigureCanvasTkAgg(fig, master=tab2)
 canvas.get_tk_widget().pack()
 
+streaming_history_label = tk.Label(tab3, text="Top 10 Artists by Milliseconds Played")
+streaming_history_label.pack()
+
+streaming_history_df = pd.DataFrame(streaming_history)
+streaming_history_df['endTime'] = pd.to_datetime(streaming_history_df['endTime'])
+
+# Calculate total msPlayed for each artist
+top_artists_df = streaming_history_df.groupby('artistName')['msPlayed'].sum().reset_index()
+top_artists_df = top_artists_df.sort_values(by='msPlayed', ascending=False).head(10)
+
+fig = Figure(figsize=(8, 6))
+ax = fig.add_subplot(111)
+ax.bar(top_artists_df['artistName'], top_artists_df['msPlayed'])
+ax.set_title('Top 10 Artists by Milliseconds Played')
+ax.set_xlabel('Artist')
+ax.set_ylabel('Total Milliseconds Played')
+ax.tick_params(axis='x', rotation=15)  # Rotate x-axis labels for better readability
+canvas = FigureCanvasTkAgg(fig, master=tab3)
+canvas.get_tk_widget().pack()
+
 # Tab 3: Inferences
-inferences_label = tk.Label(tab3, text="User Inferences")
+inferences_label = tk.Label(tab4, text="User Inferences")
 inferences_label.pack()
 
-inferences_text = tk.Text(tab3, height=20, width=40)
+inferences_text = tk.Text(tab4, height=20, width=40)
 inferences_text.insert(tk.END, '\n'.join(inferences_data['inferences']))
 inferences_text.pack()
 
